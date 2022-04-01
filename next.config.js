00f99d6ae777b73e4df9ab74e98441205fe8a5a7
zehao <mail@zehao.me>
2022-04-01 22:56:46 +0800

    fix: img domains

--- next.config.js
+++ next.config.js
@@ -3,7 +3,12 @@
 const nextConfig = {
 	reactStrictMode: true,
 	images: {
-		domains: ['img2.doubanio.com'],
+		domains: [
+			'img1.doubanio.com',
+			'img2.doubanio.com',
+			'img3.doubanio.com',
+			'img9.doubanio.com',
+		],
 	},
 	env: {
 		LEANCLOUD_APP_ID: process.env.LEANCLOUD_APP_ID,
